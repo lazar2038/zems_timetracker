e696a97907846f2fa3b34326883813ddad6a4bad
Konstantin Kochnov <k.kochnov@gmail.com>
2024-02-21 20:53:36 +0300

    Breadcrumbs finished (need fix to D.R.Y.)

--- resources/js/routes/breadcrumbs.js
+++ resources/js/routes/breadcrumbs.js
@@ -1,6 +1,15 @@
 const breadcrumbs = {
-    'projects.index' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'projects.index', 'title' : "Проекты"}],
-    'tasks.index' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'tasks.index', 'title' : "Задачи"}]
+    'projects.index' : [ {'route' : 'main', title: 'Главная'} , {'title' : "Проекты"}],
+    'projects.show' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'projects.index', 'title' : "Проекты"}, {'title' : "Страница проекта"}],
+    'projects.create' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'projects.index', 'title' : "Проекты"}, {'title' : "Создание"}],
+    'projects.edit' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'projects.index', 'title' : "Проекты"}, {'title' : "Редактирование проекта"}],
+    'projects.delete' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'projects.index', 'title' : "Проекты"}, {'title' : "Удаление проекта"}],
+    'tasks.index' : [ {'route' : 'main', title: 'Главная'} , {'title' : "Задачи"}],
+    'tasks.show' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'tasks.index', 'title' : "Задачи"}, {'title' : "Страница задачи"}],
+    'tasks.create' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'tasks.index', 'title' : "Задачи"}, {'title' : "Создание задачи"}],
+    'tasks.edit' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'tasks.index', 'title' : "Задачи"}, {'title' : "Редактирование задачи"}],
+    'tasks.delete' : [ {'route' : 'main', title: 'Главная'} , {'route' : 'tasks.index', 'title' : "Задачи"}, {'title' : "Удаление задачи"}],
+    'timelines.index' : [ {'route' : 'main', title: 'Главная'} , {'title' : "Тамлайны"}]
 
 };
 
